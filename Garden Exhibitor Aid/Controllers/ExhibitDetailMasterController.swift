845a709d77053128295f15bcffbec416ee2d1ec3
Kshitij Pandey <kshitijpandey32@gmail.com>
2020-09-07 01:19:22 +1000

    added views for exhibit detail screen

---
+++ Garden Exhibitor Aid/Controllers/ExhibitDetailMasterController.swift
@@ -0,0 +1,18 @@
+//
+//  ExhibitDetailViewMasterController.swift
+//  Garden Exhibitor Aid
+//
+//  Created by Kshitij Pandey on 07/09/20.
+//  Copyright © 2020 Kshitij Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ExhibitDetailMasterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Garden Exhibitor Aid/Controllers/PlantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantTableViewCell.swift
+//  Garden Exhibitor Aid
+//
+//  Created by Kshitij Pandey on 07/09/20.
+//  Copyright © 2020 Kshitij Pandey. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
